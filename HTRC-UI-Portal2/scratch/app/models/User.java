268e5d87d301f3e629ff5514e08748c7bb953ec1
Samitha Liyanage <samitha.liyanage@gmail.com>
2013-12-16 04:55:58 +0000

    fixing some ebean issues.

--- HTRC-UI-Portal2/scratch/app/models/User.java
+++ HTRC-UI-Portal2/scratch/app/models/User.java
@@ -50,8 +50,8 @@ public class User extends Model {
         this.accessToken = accessToken;
     }
 
-    public static Finder<String, User> find = new Finder<String, User>(
-            String.class, User.class
+    public static Finder<Long, User> find = new Finder<Long, User>(
+            Long.class, User.class
     );
 
 
