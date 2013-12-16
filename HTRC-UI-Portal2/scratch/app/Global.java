a175f46eec9a719469c2ca41be00f6e562030f2f
shliyana <shliyana@20067b5d-19cb-433b-9c3c-d0509bb56a8e>
2013-12-16 04:48:31 +0000

    changes to Global.

--- HTRC-UI-Portal2/scratch/app/Global.java
+++ HTRC-UI-Portal2/scratch/app/Global.java
@@ -15,18 +15,11 @@
  *
  */
 
-import play.*;
-import play.libs.*;
-import com.avaje.ebean.Ebean;
-import models.*;
-import java.util.*;
+import play.Application;
+import play.GlobalSettings;
 
 public class Global extends GlobalSettings {
     @Override
     public void onStart(Application app) {
-        // Check if the database is empty
-        if (User.find.findRowCount() == 0) {
-            Ebean.save((List) Yaml.load("initial-data.yml"));
-        }
     }
 }
