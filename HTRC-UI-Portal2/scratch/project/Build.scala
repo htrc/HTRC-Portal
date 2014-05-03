1c5b0c163a33be5761711f680138abe7d9d922c2
Samitha Liyanage <samitha.liyanage@gmail.com>
2014-05-03 04:46:21 +0000

    partially fixed user creation issue. only the dependency issues are fixed. need to do the runtime check.

--- HTRC-UI-Portal2/scratch/project/Build.scala
+++ HTRC-UI-Portal2/scratch/project/Build.scala
@@ -2,52 +2,44 @@ import sbt._
 import Keys._
 import play.Project._
 
-object ApplicationBuild extends Build { 
+object ApplicationBuild extends Build {
 
-  val appName = "htrc-portal"   
+  val appName = "htrc-portal"
   val appVersion = "3.0.0-SNAPSHOT"
 
   /* Add Dependencies in following format
    *  "group_id" % "artifact_id" % "version"*/
 
-    val appDependencies = Seq(
+  val appDependencies = Seq(
+    "edu.indiana.d2i.htrc.wso2is.extensions" % "edu.indiana.d2i.htrc.wso2is.extensions.stub" % "1.0.0",
     "org.apache.amber.wso2" % "amber" % "0.22.1358727.wso2v3",
     "edu.indiana.d2i.htrc.oauth2" % "client-api" % "1.0.1",
     "org.apache.amber" % "oauth2-resourceserver" % "0.22.1358727-wso2v3",
     "com.googlecode.json-simple" % "json-simple" % "1.1",
-     "mysql" % "mysql-connector-java" % "5.1.27",
+    "mysql" % "mysql-connector-java" % "5.1.27",
     "org.wso2.carbon" % "org.wso2.carbon.identity.oauth.stub" % "4.2.2",
-    "edu.illinois.i3.htrc" % "UserManager" % "0.3.1"
-      exclude("org.apache.tomcat.ha.wso2","tomcat-ha")
-      exclude("org.wso2.securevault","org.wso2.securevault")
-      exclude("org.wso2.carbon","org.wso2.carbon.registry.extensions")
-      exclude("org.apache.maven.scm.wso2","maven-scm")
-      exclude("org.wso2.carbon","org.wso2.carbon.ui")
-      exclude("ch.qos.logback","logback-classic")
-      exclude("commons-fileupload.wso2","commons-fileupload")
-      exclude("commons-codec.wso2","commons-codec")
-      exclude("commons-collections.wso2","commons-collections")
-      exclude("commons-pool.wso2","commons-pool")
-      exclude("commons-beanutils.wso2","commons-beanutils")
-      exclude("org.jboss.marshalling.wso2","marshalling")
-      exclude("org.jboss.logging.wso2","jboss-logging")
-      exclude("org.wso2.carbon","org.wso2.carbon.caching.core")
-      exclude("org.wso2.carbon","org.wso2.carbon.ntask.core")
-      exclude("org.slf4j","jcl-over-slf4j"),
-
+    "org.wso2.carbon" % "org.wso2.carbon.user.mgt.stub" % "4.2.0",
+    "org.wso2.carbon" % "org.wso2.carbon.registry.ws.client" % "4.2.0"
+	exclude("org.eclipse.core","org.eclipse.core.runtime")
+	exclude("uddi","uddi4j")
+	exclude("org.apache.maven.scm", "maven-scm-api"),
+    "org.wso2.carbon" % "org.wso2.carbon.registry.core" % "4.2.0" 
+	exclude("org.eclipse.core","org.eclipse.core.runtime")
+	exclude("uddi","uddi4j")
+	exclude("org.apache.maven.scm", "maven-scm-api"),
+    "org.wso2.carbon" % "org.wso2.carbon.registry.extensions.stub" % "4.2.0",
+    "org.wso2.carbon" % "org.wso2.carbon.identity.authenticator.token.stub" % "4.2.0",
     javaCore,
     javaJdbc,
     javaEbean
   )
 
-/* Add repositories in following format
-* "repository_name" at "repository path"*/
+  /* Add repositories in following format
+  * "repository_name" at "repository path"*/
 
-   val main = play.Project(appName, appVersion, appDependencies).settings(
-     resolvers += ("WSO2 Maven Repository" at "http://maven.wso2.org/nexus/content/groups/wso2-public"),
-     resolvers += ("Local Maven Repository" at "file://"+ Path.userHome.absolutePath + "/.m2/repository"),
-    resolvers += "Internal Release Repository" at "http://htrc.illinois.edu:8080/archiva/repository/internal/",
-    resolvers += "Internal Snapshot Repository" at "http://htrc.illinois.edu:8080/archiva/repository/snapshots/"
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    resolvers += ("WSO2 Maven Repository" at "http://maven.wso2.org/nexus/content/groups/wso2-public/"),
+    resolvers += ("Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository")
   )
 
 }
