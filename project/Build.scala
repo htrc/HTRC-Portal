1cfdd20f66a20083706643e2081542e8201c9b73
samithaliyanage <samitha.liyanage@gmail.com>
2015-01-05 17:13:20 -0500

    Changes on backend to compatible with SAML SSO

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,14 @@ object ApplicationBuild extends Build {
   val appVersion = "3.0.0-SNAPSHOT"
 
   val appDependencies = Seq(
+    javaCore,
+    javaJdbc,
+    javaEbean,
+    cache,
+    "org.pac4j" % "play-pac4j_java" % "1.2.1.htrcv1"
+   exclude("com.typesafe.play", "play-jdbc")
+   exclude("com.typesafe.play", "play-cache"),
+    "org.pac4j" % "pac4j-saml" % "1.5.1.htrcv2",
     "edu.indiana.d2i.htrc.security" % "useradmin-ext-stub" % "1.0.0-SNAPSHOT",
     "org.apache.amber.wso2" % "amber" % "0.22.1358727.wso2v3",
     "edu.indiana.d2i.htrc.oauth2" % "client-api" % "1.0.1",
@@ -27,11 +35,8 @@ object ApplicationBuild extends Build {
 	exclude("uddi","uddi4j")
 	exclude("org.apache.maven.scm", "maven-scm-api"),
     "org.wso2.carbon" % "org.wso2.carbon.registry.extensions.stub" % "4.2.0",
-    "org.wso2.carbon" % "org.wso2.carbon.identity.authenticator.token.stub" % "4.2.0",
-    javaCore,
-    javaJdbc,
-    javaEbean
-  )
+    "org.wso2.carbon" % "org.wso2.carbon.identity.authenticator.token.stub" % "4.2.0"
+)
 
   /* Add repositories in following format
   * "repository_name" at "repository path"*/
@@ -39,7 +44,9 @@ object ApplicationBuild extends Build {
   val main = play.Project(appName, appVersion, appDependencies).settings(
     resolvers += ("WSO2 Maven Repository" at "http://maven.wso2.org/nexus/content/groups/wso2-public/"),
     resolvers += ("Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository"),
-    resolvers += ("HTRC Nexus Repository" at "http://htrc.illinois.edu/nexus/content/groups/public/" )
+    resolvers += ("HTRC Nexus Repository" at "http://htrc.illinois.edu/nexus/content/groups/public/" ),
+    resolvers += ("typesafe" at "http://repo.typesafe.com/typesafe/releases/"),
+    resolvers += Resolver.mavenLocal
   )
 
 }
