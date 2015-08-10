7155fd5cd11e93b1f9a8256059aa773b51fa14e6
Milinda Pathirage <milinda.pathirage@gmail.com>
2015-08-10 09:55:32 -0400

    added missing copy command to copy portal.sh

--- release.sh
+++ release.sh
@@ -14,6 +14,7 @@ play dist
 # Copy Dockerfile to target/docker
 mkdir -p ./target/docker
 cp Dockerfile ./target/docker
+cp portal.sh ./target/docker
 
 # Copy Portal distribution to target/docker
 cp ./target/universal/htrc-portal-3.2-SNAPSHOT.zip ./target/docker
