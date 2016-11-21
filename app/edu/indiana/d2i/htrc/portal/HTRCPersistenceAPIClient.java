4e39fb496cfd963eeb7af62e7ef2264423ae0693
ramarvab <ramarvab@gmail.com>
2016-11-21 13:31:33 -0500

    fix styling issues with data table

--- app/edu/indiana/d2i/htrc/portal/HTRCPersistenceAPIClient.java
+++ app/edu/indiana/d2i/htrc/portal/HTRCPersistenceAPIClient.java
@@ -611,7 +611,7 @@ public class HTRCPersistenceAPIClient {
 
     public List<String>  getVolumesInHtrc(JSONObject volumes) throws IOException
     {
-        //String volumesUrl = "http://localhost:8087/SGA_REST_DataIngestor/sga/dataingestor"  ;
+        //String volumesUrl = "http://localhost:8087/SGA_REST_DataIngestor/sga/dataingestor";
         String volumesUrl = PlayConfWrapper.htrcRightsAPI()+"/filter";
         StringRequestEntity entity = new StringRequestEntity(volumes.toJSONString(),"application/json","UTF-8");
         PostMethod post = new PostMethod(volumesUrl);
